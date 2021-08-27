2c3896571b02050a563026779691067b94386e62
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-27 19:07:07 -0400

    Add elevation and name to iframe popup

--- map1.py
+++ map1.py
@@ -4,13 +4,21 @@ import pandas
 data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
+name = list(data['NAME'])
 
-map = folium.Map(location=[lat[1], lon[1]], zoom_start=10, tiles='Stamen Terrain')
+html = '''
+<h4>Volcano: %s</h4>
+Height: %s m
+'''
+
+map = folium.Map(location=[lat[1], lon[1]], zoom_start=7, tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name='My Map')
 
 # When iterating through two lists, need to use the zip fn
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup='This is a Marker', icon=folium.Icon(color='blue')))
+for lt, ln, el, nm in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (nm, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='blue')))
 map.add_child(fg)
 
 map.save("Map1.html")
