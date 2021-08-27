493077c7614f298a025273d9805e9b991d4796aa
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-27 19:14:43 -0400

    Add an a tag that will google the volcano name

--- map1.py
+++ map1.py
@@ -8,7 +8,8 @@ elev = list(data['ELEV'])
 name = list(data['NAME'])
 
 html = '''
-<h4>Volcano: %s</h4>
+<h4>Volcano Info:</h4>
+Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 '''
 
@@ -17,7 +18,7 @@ fg = folium.FeatureGroup(name='My Map')
 
 # When iterating through two lists, need to use the zip fn
 for lt, ln, el, nm in zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (nm, el), width=200, height=100)
+    iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='blue')))
 map.add_child(fg)
 
