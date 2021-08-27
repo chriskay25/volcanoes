ab7bb73c090a667de6d9e136486686125b2fcd0d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-27 15:11:18 -0400

    Iterate through lat and lon to create markers for each volc

--- map1.py
+++ map1.py
@@ -8,4 +8,9 @@ lon = list(data['LON'])
 map = folium.Map(location=[lat[1], lon[1]], zoom_start=10, tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name='My Map')
 
+# When iterating through two lists, need to use the zip fn
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup='This is a Marker', icon=folium.Icon(color='blue')))
+map.add_child(fg)
+
 map.save("Map1.html")
