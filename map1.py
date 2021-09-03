3aca4842e14f972d158e503a6ae1301357582c8f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-02 22:55:39 -0400

    Add different colored markers depending on elevation

--- map1.py
+++ map1.py
@@ -13,13 +13,23 @@ Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a
 Height: %s m
 '''
 
+def color_producer(elevation):
+    if elevation < 1200:
+        return 'green'
+    elif 1200 <= elevation < 2000:
+        return 'blue'
+    elif 2000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[lat[1], lon[1]], zoom_start=7, tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name='My Map')
 
 # When iterating through two lists, need to use the zip fn
 for lt, ln, el, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='blue')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color_producer(el))))
 map.add_child(fg)
 
 map.save("Map1.html")
