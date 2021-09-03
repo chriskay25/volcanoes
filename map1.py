b58de5f93cb988ab1c6cd93deb3c9408dcd67eb6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-03 12:15:59 -0400

    Add GeoJSON polygon layer, change icon to CircleMarker

--- map1.py
+++ map1.py
@@ -23,13 +23,15 @@ def color_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[lat[1], lon[1]], zoom_start=7, tiles='Stamen Terrain')
+map = folium.Map(location=[lat[1], lon[1]], zoom_start=6, tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name='My Map')
 
 # When iterating through two lists, need to use the zip fn
 for lt, ln, el, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color_producer(el))))
-map.add_child(fg)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), color=color_producer(el), fill_opacity=0.7, fill=True))
 
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='UTF-8-sig').read())))
+
+map.add_child(fg)
 map.save("Map1.html")
